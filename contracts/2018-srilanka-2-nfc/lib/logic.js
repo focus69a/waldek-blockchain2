46226cb4005d8333f354be09d9b1fc486cf7b861
focus69a <40357086+focus69a@users.noreply.github.com>
2018-07-02 18:14:13 +0200

    poprawki w modelu

--- contracts/2018-srilanka-2-nfc/lib/logic.js
+++ contracts/2018-srilanka-2-nfc/lib/logic.js
@@ -1,21 +1,8 @@
-/**
-
- * Track the trade of a commodity from one trader to another
-
- * @param {org.example.mynetwork.Trade} trade - the trade to be processed
-
- * @transaction
-
- */
-
-async function transferNFCTag(assignTag) {
-
-    assignTag.NFCAsset.currentOwner = assignTag.newOwner;
-
-    let assetRegistry = await getAssetRegistry('org.callforcode.biznet.cto');
-
-    await assetRegistry.update(assignTag.NFCAsset);
+async function transferNFCTag(tx) {
+    const oldOwner = tx.asset.currentOwner
+    tx.asset.currentOwner = tx.newOwner
+    const assetRegistry = await getAssetRegistry('org.callforcode.biznet.NFCAsset');
+    await assetRegistry.update(tx.NFCAsset);
 
 }
-
 
