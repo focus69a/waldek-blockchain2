11892e25c15ff9de94778d21da189202657fd617
focus69a <40357086+focus69a@users.noreply.github.com>
2018-07-03 17:56:37 +0200

    sredniki

--- contracts/2018-srilanka-2-nfc/lib/logic.js
+++ contracts/2018-srilanka-2-nfc/lib/logic.js
@@ -1,6 +1,30 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+'use strict';
+/**
+ * Write your transction processor functions here
+ */
+
+/**
+ * Sample transaction
+ * @param {org.test.test1.SampleTransaction} sampleTransaction
+ * @transaction
+ */
 async function transferNFCTag(tx) {
-    const oldOwner = tx.asset.currentOwner
-    tx.asset.currentOwner = tx.newOwner
+    const oldOwner = tx.asset.currentOwner;
+    tx.asset.currentOwner = tx.newOwner;
     const assetRegistry = await getAssetRegistry('org.callforcode.biznet.NFCAsset');
     await assetRegistry.update(tx.NFCAsset);
 
